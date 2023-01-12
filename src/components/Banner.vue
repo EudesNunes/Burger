14f67c8138f616848a96f5d2e1e756eef7c8c940
EudesNunes <araujonuneseudes@gmail.com>
2023-01-12 15:21:20 -0300

    Home criada

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Banner",
+
+}
+</script>
+
+<style scoped>
+    #main-banner{
+        background-image: url('/img/burger.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center right;
+        height: 40vmax;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1{
+        color: #fff;
+        text-align: center;
+        font-size: 3.1vmax;
+        background-color: #222;
+        padding: 1% 2%;
+    }
+</style>
---
+++ src/views/PedidosView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="main-container">
+    <h1>Gerenciar Pedidos:</h1>
+    <p>dashboard</p>
+  </div>
+</template>
+
